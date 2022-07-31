9394a31feb9e0d43f52fd5c8093ac2ce4caf316a
beom-yun <scds0326@naver.com>
2022-07-31 21:18:17 +0900

    Adding Images

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+const fileInput = document.getElementById('file');
 const modeBtn = document.getElementById('mode-btn');
 const destroyBtn = document.getElementById('destroy-btn');
 const eraseBtn = document.getElementById('eraser-btn');
-const colorOptions = Array.from(
-  document.getElementsByClassName('color-option')
-);
+const colorOptions = Array.from(document.getElementsByClassName('color-option'));
 const color = document.getElementById('color');
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
@@ -79,6 +78,17 @@ function onEraserClick() {
   modeBtn.innerText = 'Fill';
 }
 
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
@@ -93,3 +103,4 @@ colorOptions.forEach(color => color.addEventListener('click', onColorClick));
 modeBtn.addEventListener('click', onModeClick);
 destroyBtn.addEventListener('click', onDestroyClick);
 eraseBtn.addEventListener('click', onEraserClick);
+fileInput.addEventListener('change', onFileChange);
