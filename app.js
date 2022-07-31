e0226a103b041f760a3661cff175a5adaaa68839
beom-yun <58946778+beom-yun@users.noreply.github.com>
2022-07-31 09:39:24 +0900

    Paint Color part Two

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName('color-option')
+);
 const color = document.getElementById('color');
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
@@ -36,6 +39,12 @@ function onColorChange(event) {
   ctx.fillStyle = event.target.value;
 }
 
+function onColorClick(event) {
+  ctx.strokeStyle = event.target.dataset.color;
+  ctx.fillStyle = event.target.dataset.color;
+  color.value = event.target.dataset.color;
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
@@ -43,3 +52,5 @@ canvas.addEventListener('mouseleave', cancelPainting);
 
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener('change', onColorChange);
+
+colorOptions.forEach(color => color.addEventListener('click', onColorClick));
